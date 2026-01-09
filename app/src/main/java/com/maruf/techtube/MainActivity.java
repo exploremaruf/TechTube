a158dc8268f691a34f2214aadb141a935d729903
exploremaruf <cyberistmaruf@gmail.com>
2026-01-10 02:59:55 +0600

    feat: configure permissions, dependencies, and server connection

--- app/src/main/java/com/maruf/techtube/MainActivity.java
+++ app/src/main/java/com/maruf/techtube/MainActivity.java
@@ -1,12 +1,26 @@
 package com.maruf.techtube;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Method;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +35,39 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+
+        String url = "http://192.168.1.107/apps/video.json";
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+
+            @Override
+            public void onResponse(JSONArray jsonArray) {
+
+                try {
+                    JSONObject jsonObject = jsonArray.getJSONObject(0);
+                    String title = jsonObject.getString("title");
+                    String video_id = jsonObject.getString("video_id");
+                    Log.d("serverresponse", "" + title + "\n" + "" + video_id);
+
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+
+                Log.d("serverresponse", "" + volleyError);
+
+            }
+        });
+
+        queue.add(jsonArrayRequest);
+
     }
 
 }
